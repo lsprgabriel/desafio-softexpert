81a2ba2d1fc9f5df64f164895f12740c1604fca1
pgabriel <pgabriel@softexpert.com>
2024-02-21 17:13:41 -0300

    js updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-const getCartStorage = () => JSON.parse(localStorage.getItem('db_cart')) ?? []
-const setCartStorage = (dbCart) => localStorage.setItem("db_cart", JSON.stringify(dbCart))
+const table = "cart_document"
+
+const getCartStorage = () => JSON.parse(localStorage.getItem(table)) ?? []
+const setCartStorage = (data) => localStorage.setItem(table, JSON.stringify(data))
 
 const addCartData = (data) => {
     const dbCart = getCartStorage();
@@ -24,21 +26,21 @@ const setInitialCartStorage = () => {
         let initCartData = [
             {
                 product: "Rice",
-                price: "$2.00",
-                amount: "2",
-                total: "$4.00",
+                price: 2.00,
+                amount: 2,
+                total: 4.00,
             },
             {
                 product: "Corn",
-                price: "$6.00",
-                amount: "3",
-                total: "$18.00",
+                price: 6.00,
+                amount: 3,
+                total: 18.00,
             },
             {
                 product: "Potato",
-                price: "$0.50",
-                amount: "21",
-                total: "$10.50",
+                price: 0.50,
+                amount: 21,
+                total: 10.50,
             }
         ]
         initCartData.map((data) => {
@@ -54,35 +56,44 @@ const setInitialCartStorage = () => {
 
 setInitialCartStorage();
 
+let totalPrice = 0;
+
 for(let i = 0; i < storage.length; i++) {
     let data = storage[i];
+    totalPrice = totalPrice + data.total;
     let cellData = [ data.product, data.price, data.amount, data.total,
-        `<td><button onClick='onProductDelete(${i})'>Delete</button></td>`
+        `<td><button onClick='onCartDelete(${i})'>Delete</button></td>`
     ]
-
-    var tbodyRef = document.getElementById('productTable').getElementsByTagName('tbody')[0];
-
+    
+    var tbodyRef = document.getElementById('cartTable').getElementsByTagName('tbody')[0];
+    
     var newRow = tbodyRef.insertRow();
-
+    
     for(let j = 0; j < 5; j++) {
         let cell = newRow.insertCell(j);
-        cell.innerHTML = cellData[j]
+        if (j == 1 || j == 3) {
+            cell.innerHTML = `$${cellData[j]}`
+        } else {
+            cell.innerHTML = cellData[j]
+        }
     }
+    console.log(totalPrice)
 }
 
+document.getElementById("totalPrice").value = `$${ totalPrice }`;
 
-const onProductSubmit = (form) => {
-    alert("Produto adicionado com sucesso!");
+const onCartSubmit = (form) => {
+    alert("Product successfully added!");
     
     addCartData({
         product: form.product.value,
-        price: `$${form.price.value}`,
-        amount: `$${form.amount.value}`,
-        total: `$${form.price.value * form.amount.value}`,
+        price: form.price.value,
+        amount: form.amount.value,
+        total: form.price.value * form.amount.value,
     })
 }
 
-const onProductDelete = (index) => {
+const onCartDelete = (index) => {
     deleteCartData(index);
     window.location.reload();
 }
